fbe3eeb453c2cf55c291af046a9b016e7b211cd4
Vítor <vitor_kei9@hotmail.com>
2017-08-11 00:21:12 -0300

    Lista 1, exercício 10 feito, mas falta tirar algumas dúvidas

--- Grafos/l1e10.c
+++ Grafos/l1e10.c
@@ -1,3 +1,8 @@
+/*
+ * Nome: Vítor Kei Taira Tamada
+ * NUSP: 8516250
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #define Vertex int
@@ -11,7 +16,6 @@ typedef struct graph* Graph;
 
 int** MATRIXinit(int R, int C, int E)
 {
-  printf("blub\n");
   int i, j;
   int** mat = (int**) malloc (R * sizeof(int*));
   for (i = 0; i < R; i++)
@@ -21,7 +25,6 @@ int** MATRIXinit(int R, int C, int E)
     for (j = 0; j < C; j++)
       mat[i][j] = E;
   
-  printf("bleb\n");
   return mat;
 }
 
@@ -70,50 +73,56 @@ void GRAPHshow (Graph G)
   Vertex v, w;
   for (v = 0; v < G->V; v++)
   {
-    printf ("%d: ", v);
+    printf ("%d(%c): ", v, v+97);
     for (w = 0; w < G->V; w++)
       if (G->adj[v][w] == 1)
-        printf ("%d ", w);
+        printf ("%d(%c) ", w, w+97);
     printf("\n");
   }
 }
 
 int main()
 {
-  int i, sLen;
-  char* s;
+  int i, j, sLen;
+  Vertex v, w;
+  char s[100];
   Graph G = GRAPHinit(26); /* Número de caracteres no alfabeto, desconsiderando ç e acentuados */
   
-  printf("Insira o número de caracteres que serão lidos do texto inserido: ");
+  printf("\nInsira o número de caracteres que serão lidos do texto inserido: ");
   scanf("%d", &sLen);
   
-  printf("Insira o texto que formará o grafo.\nSe o número de caracteres lidos for menor que o tamanho do texto, haverá erros.\nCaracteres maiúsculos não são diferenciados de minúsculos:\n");
+  printf("\nInsira uma string de comprimento mínimo de tamanho sLen = %d e de comprimento máximo 100 com apenas letras a-z e A-Z:\n", sLen);
   scanf("%s", s);
   
-  for (i = 0; i < sLen; i++)
+  for (i = 0; i < sLen - 1; i++)
   {
-    if (s[i] > 64 && s[i] < 91) s[i] += 32; /* Converte para minúsculo */
-    if (s[i]
+    j = 1; /* Auxiliar */
+    
+    if      (s[i] > 64 && s[i] < 91)  s[i] += 32; /* Se o caracter for A-Z, converte-o para a-z */
+    else if (s[i] < 97 || s[i] > 122) continue;   /* Se o caracter não for uma letra a-z, pula para o próximo caracter do texto */
+    
+    /* A esta altura, o caracter lido deve ser uma letra a-z */
+    
+    /* Enquanto não for A-Z nem a-z */
+    while (i+j < sLen)
+      if(!((s[i+j] > 64 && s[i+j] < 91) || (s[i+j] > 96 && s[i+j] < 123)))
+        j++; /* Procura o próximo caracter a-z ou A-Z */
+      else break;
+    
+    if (s[i+j] > 64 && s[i+j] < 91) s[i+j] += 32; /* Converte uma letra A-Z em a-z */
+    
+    /* A esta altura, s[i] e s[i+j] são duas letras a-z, independente de quais caracteres especiais haja entre eles */
+    
+    /* Para facilitar o entendimento */
+    v = s[i] - 97;
+    w = s[i+j] - 97;
+    
+    GRAPHinsertArc(G, v, w);
   }
+  
+  printf("\n");
+  GRAPHshow(G);
   
   return 0;
 }
 
-/* TESTE
-int main()
-{
-  Graph G = GRAPHinit(10);
-  int test = 123456789;
-  Vertex v, w;
-  for (v = test % 10, w = (test / 10) % 10; test / 10 != 0; test = test / 10, v = test % 10, w = (test / 10) % 10)
-  {
-    printf("%d -> %d\n", w, v);
-    GRAPHinsertArc (G, w, v);
-    printf("%d\n", test);
-  }
-  
-  GRAPHshow (G);
-  
-  return 0;
-}
-*/

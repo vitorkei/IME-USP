5b55c7f73e82c94066f7d124e29434dc1080b129
Vítor <vitor_kei9@hotmail.com>
2018-03-18 21:08:34 -0300

    [ML] Atualizando arquivo do mini mini EP1

--- ML/equacao_normal.py
+++ ML/equacao_normal.py
@@ -2,10 +2,18 @@
 
 import numpy as np
 
-n = [1, 21, 42]
-s = [[1, 5, 21], [3, 42, 7], [12, 17, 0]]
-
 def normal_equation_prediction(X, y):
+    """
+    Calculates the prediction using the normal equation method.
+    You should add a new column with 1s.
+
+    :param X: design matrix
+    :type X: np.array
+    :param y: regression targets
+    :type y: np.array
+    :return: prediction
+    :rtype: np.array
+    """
     ones = np.ones((len(X), 1))
     step1 = np.hstack((ones, X)) # adds column of 1s
     XT = np.transpose(step1)
@@ -13,6 +21,5 @@ def normal_equation_prediction(X, y):
     step3 = np.linalg.inv(step2) # (X^T * X)^{-1}
     step4 = np.dot(step3, XT)    # (X^T * X)^{-1} * X^T
     w = np.dot(step4, y)         # w = (X^T * X)^{-1} * X^T * y
+    
     return (np.dot(step1, w))
-
-normal_equation_prediction(s, n)
